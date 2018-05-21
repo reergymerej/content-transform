942d5197ce705fb186aa6b0c87345702fb1b66b0
Jeremy Greer <jeremy.greer@engageonq.com>
2018-05-21 08:44:41 -0400

    Return content without signature

--- transform-json.py
+++ transform-json.py
@@ -28,12 +28,20 @@ meta['author'] = get_meta_value(content, 'author')
 meta['date'] = get_meta_value(content, 'date')
 
 
+def has_pgp_signature(content):
+    return bool(re.search('^-----BEGIN PGP SIGNED MESSAGE-----', content))
 
+def get_content_without_signature(content):
+    is_signed_content = has_pgp_signature(content)
+    if is_signed_content:
+        pattern = '-----BEGIN PGP SIGNED MESSAGE-----\nHash:[^\n]+\n(.*)-----BEGIN PGP SIGNATURE-----'
+        match = re.search(pattern, content, flags=re.MULTILINE|re.DOTALL)
+        if match:
+            return match.group(1)
+    else:
+        return content
 
-
-
-
-parsed_content['without_signature'] = 'xxxxxxx'
+parsed_content['without_signature'] = get_content_without_signature(content)
 
 
 # spit out result
